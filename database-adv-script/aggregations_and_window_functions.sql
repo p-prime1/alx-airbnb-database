881672162d1195576a7f9f7ebcb2ce6486920904
p-prime1 <preciousnwaka95@gmail.com>
2025-05-22 23:01:30 +0100

    Added the for clause and use row_number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,5 +1,8 @@
 SELECT user_id, COUNT(bookings) AS booking_count
+FROM BOOKINGS
 GROUP BY user_id;
 
 SELECT *
-RANK() OVER (PARTITION BY properties ORDER BY booking_count DESC) AS no_bookings;
+RANK() OVER (PARTITION BY properties ORDER BY booking_count DESC) AS rank,
+ROW_NUMBER() OVER (PARTITION BY properties ORDER BY booking_count DESC) AS row_number
+FROM BOOKINGS;
