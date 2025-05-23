ec060697d9f659283cb0dfa9ad655da8ae940da6
p-prime1 <preciousnwaka95@gmail.com>
2025-05-23 14:00:01 +0100

    added the ANALYZE command to check for performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,8 +1,8 @@
-EXPLAIN SELECT Ratings FROM property
+EXPLAIN ANALYZE SELECT Ratings FROM property
 WHERE Ratings > 4.0;
 
 CREATE INDEX idx_ratings ON property(Ratings);
 
 
-EXPLAIN SELECT Ratings FROM property
+EXPLAIN ANALYZE SELECT Ratings FROM property
 WHERE Ratings > 4.0;
