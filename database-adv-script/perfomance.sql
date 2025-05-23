0caf7e66d146c74302dcbfc0767ea3d845c4dbba
p-prime1 <preciousnwaka95@gmail.com>
2025-05-23 14:12:25 +0100

    Used the WHERE and AND comand

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,8 @@
-EXPLAIN SELECT * FROM USER;
+EXPLAIN SELECT * FROM USER
+WHERE USER.property = 'house'
+AND USER.payment > 1000;
 
 EXPLAIN SELECT USER.property, USER.payment
 FROM USER
+WHERE USER.property = 'house'
+AND USER.payment > 1000;
